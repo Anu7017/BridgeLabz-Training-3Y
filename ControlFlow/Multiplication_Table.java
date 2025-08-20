6d646f06eabfd400ee1e55b26c49f8c3a68133a9
anurag sharma <anuragsharma2092003@gmail.com>
2025-08-20 11:30:55 +0530

    Create  Multiplication_Table.java

---
+++ ControlFlow/Multiplication_Table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        System.out.println("Multiplication Table of " + number + " from 6 to 9:");
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        input.close();
+    }
+}
