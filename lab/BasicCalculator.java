786a7c5ccf7fc9a20a085aa281ab7fc7607cf38f
anurag sharma <anuragsharma2092003@gmail.com>
2025-08-20 10:48:54 +0530

    Create BasicCalculator.java

---
+++ lab/BasicCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Take user input for two floating-point numbers
+        System.out.print("Enter the first number: ");
+        double number1 = input.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double number2 = input.nextDouble();
+
+        // Perform arithmetic operations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = 0;
+
+        if (number2 != 0) {
+            division = number1 / number2;
+        } else {
+            System.out.println("Division by zero is not allowed.");
+        }
+
+        // Print results
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        if (number2 != 0) {
+            System.out.println("Division: " + division);
+        }
+
+        input.close();
+    }
+}
