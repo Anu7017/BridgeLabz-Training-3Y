e12ab412fec6243430a8c4e99f766bb7513a8421
anurag sharma <anuragsharma2092003@gmail.com>
2025-08-20 11:20:21 +0530

    Create FactorialForLoop.java

---
+++ ControlFlow/FactorialForLoop.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FactorialForLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int n = input.nextInt();
+
+        if (n < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            int factorial = 1;
+
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+
+            System.out.println("The factorial of " + n + " is " + factorial);
+        }
+
+        input.close();
+    }
+}
