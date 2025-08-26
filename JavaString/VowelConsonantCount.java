90d20bfc92f6899771f1d9d7b6d474f56933ae82
anurag sharma <anuragsharma2092003@gmail.com>
2025-08-26 11:21:02 +0530

    Create VowelConsonantCount.java

---
+++ JavaString/VowelConsonantCount.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VowelConsonantCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine().toLowerCase();
+
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.isLetter(ch)) {
+                if ("aeiou".indexOf(ch) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Number of vowels: " + vowels);
+        System.out.println("Number of consonants: " + consonants);
+    }
+}
